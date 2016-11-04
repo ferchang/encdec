0f6d3b09d7a49582f1c725cb43720e394f442195
unknown <hm@hm-ebcb06a0e342.(none)>
2016-11-04 19:33:18 +0330

    backup5

--- crypto.py
+++ crypto.py
@@ -6,7 +6,7 @@ from Crypto.Protocol.KDF import PBKDF2
 
 #------------------------------
 
-BLOCK_SIZE=16 #16 => AES-128 , 32 => AES-256
+BLOCK_SIZE=32 #16 => AES-128 , 32 => AES-256
 
 COUNT=1000 #PBKDF2  repeat count
 
@@ -18,15 +18,15 @@ HMAC_LEN=32 #HMAC output length (currently, changing this needs code changes)
 
 #------------------------------
 
-pad=lambda s: s+((BLOCK_SIZE-len(s)%BLOCK_SIZE)*chr(BLOCK_SIZE-len(s)%BLOCK_SIZE)).encode()
+pad=lambda s, block_size: s+((block_size-len(s)%block_size)*chr(block_size-len(s)%block_size)).encode()
 unpad = lambda s : s[:-ord(s[len(s)-1:])]
 
-def str2key(str, salt=None, count=COUNT):
+def str2key(str, salt=None, count=COUNT, block_size=16):
 	if not salt: salt=Random.new().read(SALT_LEN)
-	keys=PBKDF2(str, salt,  dkLen=BLOCK_SIZE*2, count=count, prf=lambda password, salt: HMAC.new(password, salt, SHA256).digest())
-	return salt, keys[:BLOCK_SIZE], keys[BLOCK_SIZE:]
+	keys=PBKDF2(str, salt,  dkLen=block_size*2, count=count, prf=lambda password, salt: HMAC.new(password, salt, SHA256).digest())
+	return salt, keys[:block_size], keys[block_size:]
 
-def encrypt(data, key):
+def encrypt(data, key, algo):
 	
 	if not len(data):
 		print('no data to encrypt!')
@@ -35,15 +35,20 @@ def encrypt(data, key):
 		print('no encryption key!')
 		return None
 	
-	salt, key1, key2=str2key(key)
+	if algo=='AES-128': block_size=16
+	else: block_size=32
 	
-	data=pad(data)
+	print('block_size:', block_size)
+	
+	salt, key1, key2=str2key(key, block_size=block_size)
+	
+	data=pad(data, block_size)
 	iv=Random.new().read(AES.block_size)
 	aes=AES.new(key1, AES.MODE_CBC, iv)
 	
 	ct=iv+aes.encrypt(data)
 	
-	return str(COUNT).encode()+b'#'+salt+HMAC.new(key2, ct, SHA256).digest()+ct
+	return str(block_size).encode()+b'#'+str(COUNT).encode()+b'#'+salt+HMAC.new(key2, ct, SHA256).digest()+ct
 
 def decrypt(data, key):
 	
@@ -54,13 +59,14 @@ def decrypt(data, key):
 		print('no decryption key!')
 		return None
 	
-	count, data=data.split(b'#', 1)
+	block_size, count, data=data.split(b'#', 2)
 	
 	count=int(count)
+	block_size=int(block_size)
 	
 	salt=data[:SALT_LEN]
 	
-	salt, key1, key2=str2key(key, salt, count)
+	salt, key1, key2=str2key(key, salt, count, block_size=block_size)
 		
 	hmac1=data[SALT_LEN:SALT_LEN+HMAC_LEN]
 	ct=data[SALT_LEN+HMAC_LEN:]
--- encdec.py
+++ encdec.py
@@ -14,23 +14,37 @@ class Application(Frame):
 		
 		container=Frame(self)
 		
+		#-------------------------
+	
+		btn=Button(container, text='Password')
+		btn["command"]=lambda: self.getPassword()
+		btn.pack(side=TOP, padx=5, pady=5, fill=BOTH)
+		
+		w=OptionMenu(container, self.algo, 'AES-128', 'AES-256')
+		w.pack(side=TOP, padx=5, pady=5, fill=BOTH)
+		
+		#-------------------------
+		
 		btn=Button(container, text='Encrypt')
 		btn["command"]=lambda command='encrypt': self.btnClick(command)
-		btn.pack(side=LEFT, padx=5, pady=5)
+		btn.pack(side=TOP, padx=5, pady=5, fill=BOTH)
 		
 		btn=Button(container, text='Decrypt')
 		btn["command"]=lambda command='decrypt': self.btnClick(command)
-		btn.pack(side=LEFT, padx=5, pady=5)
+		btn.pack(side=TOP, padx=5, pady=5, fill=BOTH)
 		
 		container.pack()
 		
 #-------------------------------------------
+	def getPassword(self):
+		password=simpledialog.askstring('Password', 'Enter password:')
+		if not password: return False
+		self.password=password
+		return True
+#-------------------------------------------
 
 	def btnClick(self, command):
-		if not self.password:
-			password=simpledialog.askstring('Password', 'Enter password:')
-			if password==None or not password: return
-			else: self.password=password
+		if not self.password and not self.getPassword(): return
 		filename=askopenfilename(initialdir=self.lastFileOpenDir)
 		if not filename: return
 		self.lastFileOpenDir=os.path.dirname(filename)
@@ -39,7 +53,7 @@ class Application(Frame):
 			if filename.endswith(EXT):
 				tkinter.messagebox.showerror('Error', 'File already encrypted!')
 				return
-			ciphertext=encrypt(data, self.password)
+			ciphertext=encrypt(data, self.password, self.algo.get())
 			with open(filename+EXT, mode='wb') as f: f.write(ciphertext)
 		else:
 			plaintext=decrypt(data, self.password)
@@ -61,6 +75,9 @@ class Application(Frame):
 		
 		self.password=''
 		self.lastFileOpenDir=''
+		
+		self.algo=StringVar()
+		self.algo.set('AES-128')
 	
 		Frame.__init__(self, master)
 		self.createWidgets()
