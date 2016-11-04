d2430bbdbf03e2110126a52b3d878efcf74f09ee
unknown <hm@hm-ebcb06a0e342.(none)>
2016-11-04 21:14:24 +0330

    bakcup10

--- ask_file_overwrite.py
+++ ask_file_overwrite.py
@@ -29,7 +29,7 @@ class AskFileOverwrite(Toplevel):
 	def body(self, master):
 		frm=Frame(master)
 		frm.pack()
-		Label(frm, text='File already exists!\nEnter new filename:').pack({"side": "top", 'padx': 1, 'pady': 1})
+		Label(frm, text='File already exists!\nYou can change output filename\nor just click Save to overwrite it.').pack({"side": "top", 'padx': 1, 'pady': 1})
 		self.txt=Entry(frm)
 		self.txt.insert(0, self.filename)
 		self.txt.pack()
